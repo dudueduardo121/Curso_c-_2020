c2718be99fa5a207e21fb06a61dac11e75b259d5
dudueduardo121 <dudueduardo121@gmail.com>
2020-04-29 14:11:09 -0300

    Exemplo Upcasting e Downcasting c#

---
+++ Heranca/Entidades/ContaPoupansa.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heranca.Entidades {
+    class ContaPoupansa : Conta {
+        public double TaxadeJuros  { get; set; }
+
+        public ContaPoupansa() {
+        }
+
+        public ContaPoupansa(int numero, string titular, double saldo, double taxadeJuros) : base(numero, titular, saldo) {
+            TaxadeJuros = taxadeJuros;
+        }
+
+        public void AtualizarSaldo() {
+            Saldo = Saldo * TaxadeJuros;
+        }
+    }
+}
--- Heranca/Program.cs
+++ Heranca/Program.cs
@@ -5,9 +5,36 @@ namespace Heranca {
     class Program {
         static void Main(string[] args) {
 
-            ContaEmpresa conta = new ContaEmpresa(8010, "Edu", 100.00, 500.0);
+            Conta acc = new Conta(1001, "Edu", 0.0);
+            ContaEmpresa bcc = new ContaEmpresa(1002, "Maria", 0.0, 500.0);
 
-            Console.WriteLine(conta.Saldo);
+            //UPCASTING converte uma subclasse para uma superclasse
+
+            Conta acc1 = bcc;
+            Conta acc2 = new ContaEmpresa(1003, "bob", 0.0, 200.0);
+            Conta acc3 = new ContaPoupansa(1004, "Ana", 0.0, 0.01);
+
+            //DAWNCASTING converte uma superclasse para subclasse so e usado somente se muito necessario
+
+            ContaEmpresa acc4 = (ContaEmpresa)acc2;
+            acc4.Emprestimo(100.0);
+
+            //ContaEmpresa acc5 = (ContaEmpresa)acc3;// erro
+
+            if(acc3 is ContaEmpresa) {
+                //ContaEmpresa acc5 = (ContaEmpresa)acc3;
+                ContaEmpresa acc5 = acc3 as ContaEmpresa;
+                acc5.Emprestimo(200.0);
+                Console.WriteLine("Emprestimo");
+            }
+
+            if(acc3 is ContaPoupansa) {
+                //ContaPoupansa acc5 = (ContaPoupansa)acc3;
+                ContaPoupansa acc5 = acc3 as ContaPoupansa;
+                acc5.AtualizarSaldo();
+                Console.WriteLine("Atualizado!");
+            }
+
 
 
         }
