bad751660cb1ee73f22d56d1d1e6bf6b714e4bf9
dudueduardo121 <dudueduardo121@gmail.com>
2020-04-29 13:44:09 -0300

    Exemplo Heranca curso c#

---
+++ Heranca/Entidades/Conta.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heranca.Entidades {
+    class Conta {
+        public int numero { get; private set; }
+        public string Titular { get; private set; }
+        public double Saldo { get; protected set; }
+
+        public Conta() {
+        }
+        public Conta(int numero, string titular, double saldo) {
+            this.numero = numero;
+            Titular = titular;
+            Saldo = saldo;
+        }
+
+        public void Saque(double quantia) {
+            Saldo -= quantia;
+        }
+
+        public void Deposito(double quantia) {
+            Saldo += quantia;
+        }
+    }
+}
---
+++ Heranca/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Heranca.Entidades;
+
+namespace Heranca {
+    class Program {
+        static void Main(string[] args) {
+
+            ContaEmpresa conta = new ContaEmpresa(8010, "Edu", 100.00, 500.0);
+
+            Console.WriteLine(conta.Saldo);
+
+
+        }
+    }
+}
