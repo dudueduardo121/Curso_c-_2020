b6f6de8351e90e247a3da9bc9fee61964ab72f4f
dudueduardo121 <dudueduardo121@gmail.com>
2020-05-04 15:52:56 -0300

    Arquivos file, fileinfo

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -6,20 +6,9 @@ namespace xadrez_console {
     class Program {
         static void Main(string[] args) {
 
-            try {
+            PosicaoXadrez pos = new PosicaoXadrez('a', 1);
 
-
-                Tabuleiro tab = new Tabuleiro(8, 8);
-
-
-                tab.colocarPeca(new Torre(tab, Cor.Verde), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Azul), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Amarela), new Posicao(0, 0));
-
-                Tela.imprimirTabuleiro(tab);
-            }catch(tabuleiroException e) {
-                Console.WriteLine(e.Message);
-            }
+            Console.WriteLine(pos);
         }
     }
 }
