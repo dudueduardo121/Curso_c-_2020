bc4c4cdd628cf7b586e682c6897664450e3475cc
dudueduardo121 <dudueduardo121@gmail.com>
2020-05-05 12:06:54 -0300

    bloco using resumido

--- FileStrem/Program.cs
+++ FileStrem/Program.cs
@@ -7,22 +7,17 @@ namespace FileStrem {
 
             string path = @"c:\temp\file.txt";
 
-            StreamReader sr = null;
-
             try {
-                sr = File.OpenText(path);
-                while (!sr.EndOfStream) {
-                    string line = sr.ReadLine();
-                    Console.WriteLine(line);
+                using (StreamReader sr = File.OpenText(path)) {
+                    while (!sr.EndOfStream) {
+                        string line = sr.ReadLine();
+                        Console.WriteLine(line);
+                    }
                 }
-
-            }catch(IOException e) {
-                Console.WriteLine("Um erro ocorreu");
-                Console.WriteLine(e.Message);
             }
-
-            finally {
-                if (sr != null) sr.Close();
+            catch (IOException e) {
+                Console.WriteLine("Erro");
+                Console.WriteLine(e.Message);
             }
 
         }
