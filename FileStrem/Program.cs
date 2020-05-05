a91772cd4c893835a13bd2247533b7346b7e5a34
dudueduardo121 <dudueduardo121@gmail.com>
2020-05-05 11:58:25 -0300

    File strem resumido

--- FileStrem/Program.cs
+++ FileStrem/Program.cs
@@ -7,14 +7,15 @@ namespace FileStrem {
 
             string path = @"c:\temp\file.txt";
 
-            FileStream fs = null;
             StreamReader sr = null;
 
             try {
-                fs = new FileStream(path, FileMode.Open);
-                sr = new StreamReader(fs);
-                string line = sr.ReadLine();
-                Console.WriteLine(line);
+                sr = File.OpenText(path);
+                while (!sr.EndOfStream) {
+                    string line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+
             }catch(IOException e) {
                 Console.WriteLine("Um erro ocorreu");
                 Console.WriteLine(e.Message);
@@ -22,7 +23,6 @@ namespace FileStrem {
 
             finally {
                 if (sr != null) sr.Close();
-                if (fs != null) fs.Close();
             }
 
         }
