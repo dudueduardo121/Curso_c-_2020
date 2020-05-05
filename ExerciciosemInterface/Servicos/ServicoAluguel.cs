231f42279d2a063f0f8cc972f6fbf6347cb88261
dudueduardo121 <dudueduardo121@gmail.com>
2020-05-05 16:22:58 -0300

    primeira Etapa Locadora carros

---
+++ ExerciciosemInterface/Servicos/ServicoAluguel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using ExerciciosemInterface.Entidades;
+
+namespace ExerciciosemInterface.Servicos {
+    class ServicoAluguel {
+        public double ValorHora { get; private set; }
+        public double valorDia { get; private set; }
+
+        public ServicoAluguel(double valorHora, double valorDia) {
+            ValorHora = valorHora;
+            this.valorDia = valorDia;
+        }
+
+        public void processoFatura(AluguelCarro aluguelCarro) {
+
+        }
+    }
+}
